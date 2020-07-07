503199647a667d435c9bf46712d4c92303f5d086
Emanuele Albero <e.albero@gmail.com>
2020-07-08 00:45:12 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file {'/opt/minecraft':
      ensure => directory,
   }
-  file {'opt/minecraft/server.jar':
+  file {'/opt/minecraft/server.jar':
      ensure => file,
      source => 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
   }
