5b2b82f5096c414ad4daf6be05aaaf0fcff72ce2
Emanuele Albero <e.albero@gmail.com>
2020-07-07 18:17:34 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,14 @@
+node default {
   file { '/root/README2':
          ensure => file,
          content => 'This is a readme',
          owner   => 'root',
   }
+  
+  file { '/root/README2':
+         ensure => file,
+         content => 'This is another readme',
+         owner   => 'root',
+  }
+  
+}
