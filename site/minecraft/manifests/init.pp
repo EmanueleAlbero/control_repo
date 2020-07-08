62d8f3358c0c6ee87c672d95491e5aa7c2e8257b
Emanuele Albero <e.albero@gmail.com>
2020-07-08 09:33:59 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,10 +1,13 @@
 class minecraft {
+  $url = 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar'
+  $intall_dir = '/opt/minecraft/'
   file {'/opt/minecraft':
      ensure => directory,
   }
-  file {'/opt/minecraft/server.jar':
+  file {"${install_dir}/server.jar":
      ensure => file,
-     source => 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
+     source => $url,
+     before => Service['minecraft'],
   }
   package {'java':
       ensure => present,
@@ -20,5 +23,6 @@ class minecraft {
    service {'minecraft':
      ensure => running,
      enable => true,
+     require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
    }
 }
