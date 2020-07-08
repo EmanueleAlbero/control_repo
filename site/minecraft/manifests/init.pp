e2e29971db7124dfa0eb81bc69a4d515d4049773
Emanuele Albero <e.albero@gmail.com>
2020-07-08 09:44:57 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,6 @@
 class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
-  $intall_dir = '/opt/minecraft/'
+  $install_dir = '/opt/minecraft/'
 ){
   file {'/opt/minecraft':
      ensure => directory,
